4cf415e8300df4af46e88de59dc1418a3807461c
Chris Carpenter <chris.carpenter@wwt.com>
2023-04-26 10:56:19 -0700

    cancel waits on category removal

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Category } from '../category';
-import { Firestore, collectionData, collection, CollectionReference, doc, DocumentData, setDoc } from '@angular/fire/firestore';
-import { Observable, map } from 'rxjs';
+import { Firestore, collectionData, collection, CollectionReference, doc, DocumentData, setDoc, query, where } from '@angular/fire/firestore';
+import { Observable, first, map } from 'rxjs';
 import { deleteDoc } from '@angular/fire/firestore';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { WaitHold } from '../wait-hold';
 
 @Component({
   selector: 'app-categories',
@@ -13,25 +14,39 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 export class CategoriesComponent {
   category$: Observable<Category[]>;
   categoriesCollection: CollectionReference;
+  waitHoldCollection: CollectionReference;
   newCategoryName = "";
 
   constructor(private firestore: Firestore, private modalService: NgbModal) {
     this.categoriesCollection = collection(firestore, 'categories');
+    this.waitHoldCollection = collection(firestore, 'wait-holds');
     this.category$ = (collectionData(this.categoriesCollection, { idField: "id" }) as Observable<Category[]>).pipe(
       map(catArr => catArr.sort((catA, catB) => (catB.holding + catB.waiting) - (catA.holding + catA.waiting)))
     );
   }
 
   createCategory() {
-
     setDoc(doc(this.categoriesCollection, this.newCategoryName), <Category>{ id: this.newCategoryName, holding: 0, waiting: 0 })
       .then(() => this.newCategoryName = "");
   }
 
   deleteCategory(category: Category, deleteCategoryConfirm: any) {
     this.modalService.open(deleteCategoryConfirm).result.then(() => {
+      // delete category
       var categoryReference = doc<DocumentData>(this.categoriesCollection, category.id);
       deleteDoc(categoryReference);
+      // cancel holds for category
+      const whQuery = query(this.waitHoldCollection,
+        where("category", "==", category.id),
+        where("status", "in", ["Waiting", "Holding"]));
+      (collectionData(whQuery, { idField: "id" }) as Observable<WaitHold[]>).pipe(first()).subscribe(whArr => {
+        for (var wh of whArr) {
+          wh.status = 'Cancelled';
+          wh.updated = new Date();
+          const waitHoldReference = doc<DocumentData>(this.waitHoldCollection, wh.id);
+          setDoc(waitHoldReference, wh);
+        }
+      });
     });
   }
 }
