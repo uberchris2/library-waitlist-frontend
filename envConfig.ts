e7f58e0f854e0a1863337411dfb89e1ed6dd85d6
Chris Carpenter <chris.carpenter@wwt.com>
2023-04-25 17:50:41 -0700

    add api key

---
+++ envConfig.ts
@@ -0,0 +1,30 @@
+/* tslint:disable */
+// @ts-nocheck
+
+const { writeFile, existsSync, mkdirSync } = require('fs');
+
+const environmentFileContent = `export const environment = {
+    firebase: {
+        projectId: 'waitlist-firebase',
+        appId: '1:346705633941:web:6d3c42bfe3c7955cee319c',
+        storageBucket: 'waitlist-firebase.appspot.com',
+        apiKey: '${process.env['FIREBASE_API_KEY']}',
+        authDomain: 'waitlist-firebase.firebaseapp.com',
+        messagingSenderId: '346705633941',
+        measurementId: 'G-J92GC4CZ5N',
+    },
+};
+`;
+
+// creates the `environments` directory if it does not exist
+const envDirectory = './src/environments';
+if (!existsSync(envDirectory)) {
+    mkdirSync(envDirectory);
+}
+
+writeFile('./src/environments/environment.ts', environmentFileContent, function (err) {
+    if (err) {
+        console.log(err);
+    }
+    console.log(`wrote variables`);
+});
