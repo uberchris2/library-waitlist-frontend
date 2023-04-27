201de64e3f4b43ef9af88044eb61406eae7e7e26
Chris Carpenter <chris.carpenter@wwt.com>
2023-04-27 09:04:54 -0700

    handle login-logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Auth, signInWithRedirect, user, User } from '@angular/fire/auth';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from '@firebase/auth';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   isNavbarCollapsed = true;
+  user: User | undefined | null;
+
+  constructor(private auth: Auth) {
+    var user$ = user(auth);
+    user$.subscribe(u => {
+      this.user = u;
+      if (u == null) {
+        var provider = new GoogleAuthProvider();
+        provider.addScope("https://www.googleapis.com/auth/cloud-platform");
+        provider.addScope("https://www.googleapis.com/auth/datastore");
+        var auth = getAuth();
+        signInWithRedirect(auth, provider);
+      }
+    });
+  }
+
+  logout() {
+    this.auth.signOut();
+  }
+
 }
